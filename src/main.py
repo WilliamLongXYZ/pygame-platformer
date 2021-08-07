5b22e0736ab3e6bec36fa21a5ca477f8b3122616
William Long <will@williamlong.xyz>
2021-08-07 20:42:03 +0000

    Cleaned up code slightly and added main_menu() function, which is currently empty.

--- src/main.py
+++ src/main.py
@@ -1,32 +1,29 @@
-import pygame, sys # import pygame and sys
+import pygame
+from pygame.locals import *
+import sys
 
-clock = pygame.time.Clock() # set up the clock
 
-from pygame.locals import * # import pygame modules
-pygame.init() # initiate pygame
+pygame.init()
 
-pygame.display.set_caption('Pygame Window') # set the window name
-
-WINDOW_SIZE = (600,400) # set up window size
-
-screen = pygame.display.set_mode(WINDOW_SIZE,0,32) # initiate screen
-
+pygame.display.set_caption('Pygame Window')
+clock = pygame.time.Clock()
+WINDOW_SIZE = (600,400)
+screen = pygame.display.set_mode(WINDOW_SIZE,0,32)
 display = pygame.Surface((300, 200))
 
 
 player_image = pygame.image.load('images/sprites/player.png').convert()
 player_image.set_colorkey((255, 255, 255))
-sword_image = pygame.image.load('images/sprites/sword.png').convert()
-sword_image.set_colorkey((255, 255, 255))
 
-grass_image = pygame.image.load('images/tiles/grass.png')
+grass_image = pygame.image.load('images/tiles/grass.png').convert()
+dirt_image = pygame.image.load('images/tiles/dirt.png').convert()
 TILE_SIZE = grass_image.get_width()
 
-dirt_image = pygame.image.load('images/tiles/dirt.png')
+target_fps = 60
 
 game_map = [['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
             ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
-            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','2','0','0','0','0','0'],
             ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
             ['0','0','0','0','0','0','0','2','2','2','2','2','0','0','0','0','0','0','0'],
             ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
@@ -39,11 +36,7 @@ game_map = [['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'
             ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1']]
 
 def collision_test(rect, tiles):
-    hit_list = []
-    for tile in tiles:
-        if rect.colliderect(tile):
-            hit_list.append(tile)
-    return hit_list
+    return [tile for tile in tiles if rect.colliderect(tile)]
 
 def move(rect, movement, tiles):
     collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
@@ -67,73 +60,99 @@ def move(rect, movement, tiles):
             collision_types['top'] = True
     return rect, collision_types
 
-moving_right = False
-moving_left = False
 
-player_y_momentum = 0
-air_timer = 0
 
-player_rect = pygame.Rect(50, 50, player_image.get_width(), player_image.get_height())
-sword_rect =  pygame.Rect(50, 50, sword_image.get_width(), sword_image.get_height())
-test_rect = pygame.Rect(100,100,100,50)
+def main():
 
-while True: # game loop
-    display.fill((146,244,255))
+    moving_right = False
+    moving_left = False
 
-    tile_rects = []
-    y = 0
-    for row in game_map:
-        x = 0
-        for tile in row:
-            if tile == '1':
-                display.blit(dirt_image, (x * TILE_SIZE, y * TILE_SIZE))
-            if tile == '2':
-                display.blit(grass_image, (x * TILE_SIZE, y * TILE_SIZE))
-            if tile != '0':
-                tile_rects.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
-            x += 1
-        y += 1
+    player_y_momentum = 0
+    air_timer = 0
 
-    player_movement = [0, 0]
-    if moving_right:
-        player_movement[0] += 2
-    if moving_left:
-        player_movement[0] -= 2
-    player_movement[1] += player_y_momentum
-    player_y_momentum += 0.2
-    if player_y_momentum > 3:
-        player_y_momentum = 3
+    player_rect = pygame.Rect(50, 50, player_image.get_width(), player_image.get_height())
+    test_rect = pygame.Rect(100,100,100,50)
 
-    player_rect, collisions = move(player_rect, player_movement, tile_rects)
+    while True:
+        display.fill((146,244,255))
 
-    if collisions['bottom']:
-        player_y_momentum = 0
-        air_timer = 0
-    else:
-        air_timer += 1
+        tile_rects = []
+        y = 0
+        for row in game_map:
+            x = 0
+            for tile in row:
+                if tile == '1':
+                    display.blit(dirt_image, (x * TILE_SIZE, y * TILE_SIZE))
+                if tile == '2':
+                    display.blit(grass_image, (x * TILE_SIZE, y * TILE_SIZE))
+                if tile != '0':
+                    tile_rects.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+                x += 1
+            y += 1
 
-    display.blit(player_image, (player_rect.x, player_rect.y))
-    display.blit(sword_image, (sword_rect.x, sword_rect.y))
+        player_movement = [0, 0]
+        if moving_right:
+            player_movement[0] += 2
+        if moving_left:
+            player_movement[0] -= 2
+        player_movement[1] += player_y_momentum
+        player_y_momentum += 0.2
+        if player_y_momentum > 3:
+            player_y_momentum = 3
 
-    for event in pygame.event.get(): # event loop
-        if event.type == QUIT: # check for window quit
-            pygame.quit() # stop pygame
-            sys.exit() # stop script
-        if event.type == KEYDOWN:
-            if event.key == K_RIGHT:
-                moving_right = True
-            if event.key == K_LEFT:
-                moving_left = True
-            if event.key == K_UP:
-                if air_timer < 6:
-                    player_y_momentum = -5
-        if event.type == KEYUP:
-            if event.key == K_RIGHT:
-                moving_right = False
-            if event.key == K_LEFT:
-                moving_left = False
-    surf = pygame.transform.scale(display, WINDOW_SIZE)
-    screen.blit(surf, (0, 0))
-    pygame.display.update() # update display
-    clock.tick(60) # maintain 60 fps
-    
+        player_rect, collisions = move(player_rect, player_movement, tile_rects)
+
+        if collisions['bottom']:
+            player_y_momentum = 0
+            air_timer = 0
+        elif collisions["top"]:
+            player_y_momentum = 0
+        else:
+            air_timer += 1
+
+        display.blit(player_image, (player_rect.x, player_rect.y))
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key in [K_RIGHT, K_d]:
+                    moving_right = True
+                if event.key in [K_LEFT, K_a]:
+                    moving_left = True
+                if event.key in [K_UP, K_w]:
+                    if air_timer < 6:
+                        player_y_momentum = -5
+                if event.key == K_ESCAPE:
+                    main_menu()
+            if event.type == KEYUP:
+                if event.key in [K_RIGHT, K_d]:
+                    moving_right = False
+                if event.key in [K_LEFT, K_a]:
+                    moving_left = False
+        surf = pygame.transform.scale(display, WINDOW_SIZE)
+        screen.blit(surf, (0, 0))
+        pygame.display.update()
+        clock.tick(target_fps)
+
+def main_menu():
+    quit = False
+    while True:
+        display.fill((146,244,255))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    quit = True
+            if event.type == KEYUP:
+                pass
+        if quit: break
+        screen.blit(pygame.transform.scale(display, WINDOW_SIZE), (0, 0))
+        pygame.display.update()
+        clock.tick(target_fps)
+
+if __name__ == "__main__":
+    main()
